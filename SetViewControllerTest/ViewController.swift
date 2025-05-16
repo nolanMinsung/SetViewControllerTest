7fc61049f38b397d32fd0f1ab93d322ecea07c1e
Minsung Kim <mskim4048@naver.com>
2025-05-16 22:45:00 +0900

    Initial Commit

---
+++ SetViewControllerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SetViewControllerTest
+//
+//  Created by 김민성 on 5/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
